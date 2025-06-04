58c14c314fec9bcab9b80588cbdc18afd030c79c
jnicomorell <78945145+jnicomorell@users.noreply.github.com>
2025-06-04 17:35:11 -0300

    refactor accordion template

---
+++ Magezon/PageBuilder/ViewModel/AccordionData.php
@@ -0,0 +1,17 @@
+<?php
+declare(strict_types=1);
+
+namespace Magezon\PageBuilder\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magezon\Core\Helper\Data as CoreHelper;
+
+class AccordionData implements ArgumentInterface
+{
+    public function __construct(private CoreHelper $coreHelper) {}
+
+    public function filter(string $text): string
+    {
+        return $this->coreHelper->filter($text);
+    }
+}
