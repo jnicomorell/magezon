d546b0fb92c25c1a7d85237b0d2910975707e87d
jnicomorell <78945145+jnicomorell@users.noreply.github.com>
2025-06-04 17:42:13 -0300

    refactor: use view model for raw js

---
+++ Magezon/PageBuilder/ViewModel/RawJs.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace Magezon\PageBuilder\ViewModel;
+
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use Magezon\Core\Helper\Data as CoreHelper;
+
+class RawJs implements ArgumentInterface
+{
+    public function __construct(private CoreHelper $coreHelper) {}
+
+    public function getFilteredContent(string $content): string
+    {
+        return $this->coreHelper->filter($content);
+    }
+}
+
